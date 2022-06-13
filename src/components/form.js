e95f7ef554efc1cdcfbf0cadb179af62ca77c3e6
LEPSVISION <LEPSVIDZE@GMAIL.COM>
2022-06-13 21:53:48 +0300

    new

--- src/components/form.js
+++ src/components/form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = (props) => {
        return(
             <form onSubmit={props.weatherMethod}>
-                <input type="text" name="city" placeholder="Город" />
+                <input type="text" name="city" placeholder="Город" autoComplete="off"/>
                 <button>Получить погоду!</button>
             </form>
             
